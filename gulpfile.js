34c015df3bb73de90acebd5dfa289fa1b4407911
Maksim Pecherskiy <max@maksimize.com>
2017-02-20 17:48:45 -0800

    Add surge ignore.

--- gulpfile.js
+++ gulpfile.js
@@ -73,7 +73,7 @@ gulp.task("fonts", function () {
 
 // Copy xml and txt files to the "site" directory
 gulp.task("copy", function () {
-  return gulp.src(["serve/*.txt", "serve/*.xml", "serve/*.json", "src/CNAME"])
+  return gulp.src(["serve/*.txt", "serve/*.xml", "serve/*.json", "src/CNAME", "src/.surgeignore"])
     .pipe(gulp.dest("site"))
     .pipe($.size({ title: "xml, txt, json" }))
 });
