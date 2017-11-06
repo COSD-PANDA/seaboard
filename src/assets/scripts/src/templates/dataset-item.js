0b92532c0dae5b85f730376f4feeaf031cfdd2d8
Andrell Bower <bowera@gmail.com>
2017-11-06 11:57:41 -0800

    Reorder datasets by date modified

--- src/assets/scripts/src/templates/dataset-item.js
+++ src/assets/scripts/src/templates/dataset-item.js
@@ -1,6 +1,7 @@
 export default (data) => (
 `<dataset>
   <h3><a href="${data.url}">${data.title}</a></h3>
+  <p class="dataset-modified">Last updated: ${data.modified}</p>
   ${data.notes || ''}
 </dataset>`
 )
