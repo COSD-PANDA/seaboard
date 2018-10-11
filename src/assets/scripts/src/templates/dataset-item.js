c3c95687bb8088cbcb87348b7c44da46a0a4461b
Andrell Bower <bowera@gmail.com>
2018-10-11 10:34:26 -0700

    Adding date modified label to item js again

--- src/assets/scripts/src/templates/dataset-item.js
+++ src/assets/scripts/src/templates/dataset-item.js
@@ -1,7 +1,7 @@
 export default (data) => (
 `<dataset>
   <h3><a href="${data.url}">${data.title}</a></h3>
-  <p>${data.modified || ''}</p>
+  <p>Last modified: ${data.modified || ''}</p>
   ${data.notes || ''}
 </dataset>`
 )
