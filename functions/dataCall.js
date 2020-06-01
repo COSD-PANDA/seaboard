b21621bc5d8b980b1491fe6db2c6a300cf718df0
Andrell Bower <bowera@gmail.com>
2020-05-31 21:39:09 -0700

    Testing AWS SDK

--- functions/dataCall.js
+++ functions/dataCall.js
@@ -1,6 +1,21 @@
+const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY;
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_KEY;
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
 exports.handler = function(event, context, callback) {
+    
+	s3.listBuckets(function(err, data) {
+	  if (err) {
+	    console.log("Error", err);
+	  } else {
+	    console.log("Success", data.Buckets);
+	  }
+	});
+
     callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: "S3 request succesful"
     });
 }
