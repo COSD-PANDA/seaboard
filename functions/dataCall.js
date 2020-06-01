9c881fe8c34ba7d87337ad470a80a9c2f68c3893
Andrell Bower <bowera@gmail.com>
2020-06-01 14:38:05 -0700

    Adding S3 get object code

--- functions/dataCall.js
+++ functions/dataCall.js
@@ -1,18 +1,25 @@
 const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY;
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_KEY;
 const AWS = require('aws-sdk');
-AWS.config.update({region: 'us-east-1'});
+AWS.config.update({region: 'us-west-2'});
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+let s3Params = {
+	Bucket: 'datasd1-dev'
+}
 
 exports.handler = function(event, context, callback) {
+
+	s3Params.Key = 'budget/actuals_capital_FY17_datasd.csv';
     
-	s3.listBuckets(function(err, data) {
-	  if (err) {
-	    console.log("Error", err);
-	  } else {
-	    console.log("Success", data.Buckets);
-	  }
-	});
+	s3.getObject(s3Params, function(err, data) {
+      if (err)
+        return res
+          .status(500)
+          .json({error: 'Has occurred an error, please try later.'})
+
+      let object = JSON.parse(data.Body.toString());
+      console.log("Got object");
+  	})
 
     callback(null, {
     statusCode: 200,
