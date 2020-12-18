8a5aa97fd65a5bc7bcde3e1ce9490fe73342744a
Andrell Bower <bowera@gmail.com>
2020-12-18 12:36:24 -0800

    Updating gitignore and baseURL

--- public/assets/scripts/src/templates/dataset-item.js
+++
@@ -1,7 +0,0 @@
-export default (data) => (
-`<dataset>
-  <h2><a href="${data.url}">${data.title}</a></h2>
-  <p class="item-date">Last modified: ${data.modified || ''}</p>
-  ${data.notes || ''}
-</dataset>`
-)
